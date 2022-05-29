915b43558e079caead559190ef0d98a56f1f9904
Muhammad Luthfi Khusyasy <khusyasy@gmail.com>
2022-05-29 23:57:21 +0700

    [ADD] create client start

---
+++ client/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Tic Tac Toe Multiplayer Game</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+});
+</script>
+
+<style>
+</style>
