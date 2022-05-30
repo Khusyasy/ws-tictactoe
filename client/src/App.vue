64cb3607b4cb4ca60f419fdce807dde53e3b4c73
Muhammad Luthfi Khusyasy <khusyasy@gmail.com>
2022-05-30 10:59:50 +0700

    setup for play game

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,13 +2,16 @@
   <n-space justify="center" align="center" style="height: 100vh;">
     <n-card>
       <n-h1>Tic Tac Toe Multiplayer Game</n-h1>
-      <join-game />
+      <play-game v-if="store.user" />
+      <join-game v-else />
     </n-card>
   </n-space>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+
+import store from './store';
 
 import {
   NH1,
@@ -17,6 +20,7 @@ import {
 } from 'naive-ui';
 
 import JoinGame from './components/JoinGame.vue';
+import PlayGame from './components/PlayGame.vue';
 
 export default defineComponent({
   name: 'App',
@@ -25,6 +29,12 @@ export default defineComponent({
     NCard,
     NSpace,
     JoinGame,
+    PlayGame,
+},
+  setup() {
+    return {
+      store,
+    }
   },
 });
 </script>
---
+++ client/src/components/GameBoard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{ board }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'GameBoard',
+  components: {
+  },
+  props: {
+    board: Array,
+  },
+});
+</script>
+
+<style>
+</style>
