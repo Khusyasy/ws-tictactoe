f3b3bdd30b392b83f5e1feef822afaebe3143881
Muhammad Luthfi Khusyasy <khusyasy@gmail.com>
2022-05-30 10:59:32 +0700

    [MODIFY] store room

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,11 +1,41 @@
 import { ref } from 'vue'
 
 type Store = {
-  user: Record<string, unknown> | null
+  user: {
+    username: string,
+    roomId: string,
+  } | null,
+  room: {
+    id: string,
+    users: string[],
+    state: {
+      status: string,
+      turn: string | null,
+      board: string[][] | null[][],
+    }
+  } | null,
 }
 
 const store = ref<Store>({
-  user: null,
+  // user: null,
+  // room: null,
+  user: {
+    username: 'test',
+    roomId: 'asdfg',
+  },
+  room: {
+    id: 'asdfg',
+    users: [],
+    state: {
+      status: 'waiting',
+      turn: null,
+      board: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+      ],
+    },
+  },
 })
 
 export default store
